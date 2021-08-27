5a9222593da9116429dae6738b8d7704041ad8fa
Ian Louzada <ianlouzad4@gmail.com>
2021-08-26 22:34:31 -0300

    Camera now follows character

--- js/scenes/mainScene.js
+++ js/scenes/mainScene.js
@@ -6,11 +6,15 @@ class MainScene extends Phaser.Scene {
     //Pré carrega todos os assets necessários na Scene
     preload() {
         this.load.spritesheet('character', 'assets/images/character.png', { frameWidth: 48, frameHeight: 64 }, 12);
+        this.load.audio('bgmusic', 'assets/audio/bgmusic.mp3');
     }
 
     //Executa uma única vez quando a Scene é startada
     create() {
+        //this.sound.play('bgmusic', {volume: 0.05});
         this.character = new Character(this, 100, 100);
+
+        this.cameras.main.startFollow(this.character);
     }
 
     //Executa a cada frame
